114032405720642413589af57f5d9fdd843cfbda
Rafał Głowacz <rafal.glowacz@netcentric.biz>
2025-06-27 14:44:12 +0200

    Playing with git diff via jgit - 4

--- src/main/java/dev/streamx/githhub/action/WebResourceAction.java
+++ src/main/java/dev/streamx/githhub/action/WebResourceAction.java
@@ -2,12 +2,15 @@ package dev.streamx.githhub.action;
 
 import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubaction.Context;
 import io.quarkiverse.githubaction.Inputs;
 import io.quarkiverse.githubapp.event.PullRequest;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
@@ -34,16 +37,24 @@ public class WebResourceAction {
 
   @Action("pull_request")
   void webresourceSyncOnPullRequestMerged(Commands commands, Inputs inputs,
-      @PullRequest GHEventPayload.PullRequest payload) throws IOException {
+      @PullRequest GHEventPayload.PullRequest payload, Context context) throws IOException {
     commands.appendJobSummary(
         ":wave: Webresource Sync On Pull Request Merged from Quarkus GitHub Action");
     commands.notice("Here webresourceSyncOnPullRequestMerged from Quarkus GitHub Action");
 
+    commands.notice("commands: " + Objects.isNull(commands));
+    commands.notice("inputs: " + Objects.isNull(inputs));
+    commands.notice("payload: " + Objects.isNull(payload));
+    commands.notice("context: " + Objects.isNull(context));
+
+    String jsonInputs = System.getenv("JSON_INPUTS");
+    commands.notice("jsonInputs: " + jsonInputs);
+
     int commits = payload.getPullRequest().getCommits();
     commands.notice("Number of commits: " + commits);
 
-    String githubToken = inputs.getRequired("GITHUB_TOKEN");
-    commands.notice("GithubToken: " + githubToken);
+    Optional<String> githubToken = inputs.get("GITHUB_TOKEN");
+    commands.notice("GithubToken: " + (githubToken.isPresent() ?  githubToken.get() : "EMPTY_GITHUB_TOKEN"));
 
 //    CloneCommand cloneCommand = Git.cloneRepository()
 //        .setDirectory(Path.of("./repo").toFile())
