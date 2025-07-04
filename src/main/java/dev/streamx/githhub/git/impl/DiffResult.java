f7b73fcc52df02935f419267dc90f3f04287cc26
Rafał Głowacz <rafal.glowacz@ds.pl>
2025-07-04 18:06:34 +0200

    Workflow debuging - 7

--- src/main/java/dev/streamx/githhub/git/impl/DiffResult.java
+++ src/main/java/dev/streamx/githhub/git/impl/DiffResult.java
@@ -1,21 +1,26 @@
 package dev.streamx.githhub.git.impl;
 
+import dev.streamx.githhub.action.WebResourceAction;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.jboss.logging.Logger;
 
 public class DiffResult {
 
   public static final DiffResult EMPTY_RESULT = new DiffResult();
+
+  private static final Logger log = Logger.getLogger(DiffResult.class);
 
   private final Set<DiffEntry> updated = new HashSet<>();
 
   private final Set<DiffEntry> deleted = new HashSet<>();
 
   void add(DiffEntry entry) {
+    log.debug("Adding entry: " + entry);
     Optional.ofNullable(entry)
         .ifPresent(e -> {
           if (ChangeType.DELETE.equals(e.getChangeType())) {
